03e42cfdcffed4be95da839e220c54e24f94bfe8
kun <niuyukunwork@163.com>
2017-05-24 09:26:52 +0800

    Initial commit

---
+++ sanyida/src/main/java/com/baituoyitong/kun/baituoyitong/mainpackage/kedaxunfeicontrol/UnderstanderSettings.java
@@ -0,0 +1,11 @@
+package com.baituoyitong.kun.baituoyitong.mainpackage.kedaxunfeicontrol;
+
+/**
+ * com.baituoyitong.kun.baituoyitong.mainpackage.kedaxunfeicontrol
+ * <p>
+ * Created by ${kun}
+ * 2017/5/23
+ */
+public class UnderstanderSettings {
+    public static final String PREFER_NAME = "com.iflytek.setting";
+}
